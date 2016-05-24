516cc83e335f86b0e2ca4e2f6ca2263ea135b66f
tracer-ics2015 <tracer@njuics.org>
2016-05-24 22:21:29 +0800

    >  compile NEMU 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:21:29 up 41 days,  1:28,  2 users,  load average: 0.01, 0.05, 0.05 42c5c6aab8628d80aa689bdc0dd2d219db97c5f

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -5,19 +5,22 @@
 static void do_execute() {
 	uint8_t opcode = ops_decoded.opcode;
 	bool cond = false;
+	const char *opName;
 	if(DATA_BYTE != 1)
 		opcode -= 0x10;
 	switch(opcode) {
-		case 0x74:
-			// JE
-			cond = cpu.eflags_zf;
+#define SUBOP(name, condition) opName = str(name); cond = (condition); break
+		case 0x74: SUBOP(JE, cpu.eflags_zf);
 		default:
 			panic("not implemented");
 	}
+
 	if(cond) {
 		cpu.eip += (DATA_TYPE_S)op_src->val;
+		print_asm("%s 0x%x (jumped)", opName, op_src->val);
+	} else {
+		print_asm("%s 0x%x (not jumped)", opName, op_src->val);
 	}
-	print_asm_template1();
 }
 
 make_instr_helper(i)
