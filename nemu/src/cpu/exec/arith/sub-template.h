7c33e32026e8a2573ed7fcd64d2bd5cbaa547afc
tracer-ics2015 <tracer@njuics.org>
2016-06-10 21:18:56 +0800

    >  compile NEMU 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:18:56 up 10 days, 15:43,  3 users,  load average: 0.04, 0.12, 0.13 ea97500290bf65d570d9381fa895e94fb2a73dcb

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -5,17 +5,11 @@
 static void do_execute() {
 	DATA_TYPE a = op_dest->val;
 	DATA_TYPE b = op_src->val;
-	DATA_TYPE_S real_val;
 	DATA_TYPE res;
-	switch(op_src->size) {
-		case 1: real_val = (int8_t)(b); break;
-		case 2: real_val = (int16_t)(b); break;
-		case 4: real_val = (int32_t)(b); break;
-		default: panic("cmp_src_size_err");
-	}
 	if(op_src->size == 1 && op_dest->size >= 2) {
-		res = a + (-real_val);
-		cpu.eflags_of = (MSB(a) ^ MSB(real_val)) && (MSB(a) ^ MSB(res));
+		res = a + (-(int8_t)b);
+		bool sgn = b & 0x80;
+		cpu.eflags_of = (MSB(a) ^ sgn) && (MSB(a) ^ MSB(res));
 	} else {
 		res = a - b;
 		cpu.eflags_of = (MSB(a) ^ MSB(b)) && (MSB(a) ^ MSB(res));
