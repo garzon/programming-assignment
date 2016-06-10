08f463ca8eb2d760bf2743795701aad33730329a
tracer-ics2015 <tracer@njuics.org>
2016-06-10 20:32:13 +0800

    >  compile NEMU 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:32:13 up 10 days, 14:56,  3 users,  load average: 0.10, 0.09, 0.05 3604aecd280338e0459c7f30c485972967643ac

--- nemu/src/cpu/exec/control/cmp-template.h
+++ nemu/src/cpu/exec/control/cmp-template.h
@@ -5,15 +5,17 @@
 static void do_execute() {
 	DATA_TYPE a = op_dest->val;
 	DATA_TYPE b = op_src->val;
+	DATA_TYPE_S real_val;
 	DATA_TYPE res;
 	switch(op_src->size) {
-		case 1: res = a + (int8_t)(-b); break;
-		case 2: res = a + (int16_t)(-b); break;
-		case 4: res = a + (int32_t)(-b); break;
+		case 1: real_val = (int8_t)(-b); break;
+		case 2: real_val = (int16_t)(-b); break;
+		case 4: real_val = (int32_t)(-b); break;
 		default: panic("cmp_src_size_err");
 	}
+	res = a + (-real_val);
 	cpu.eflags_cf = a < b;
-	cpu.eflags_of = (MSB(a) ^ MSB(b)) && (MSB(a) ^ MSB(res));
+	cpu.eflags_of = (MSB(a) ^ MSB(real_val)) && (MSB(a) ^ MSB(res));
 	cpu.eflags_sf = MSB(res);
 	cpu.eflags_zf = res == 0;
 	set_pf(res);
