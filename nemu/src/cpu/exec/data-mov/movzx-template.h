0973292a71c98390a192662cf71f0061f26f1344
tracer-ics2015 <tracer@njuics.org>
2016-06-10 17:20:52 +0800

    >  compile NEMU 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  17:20:52 up 10 days, 11:45,  3 users,  load average: 0.25, 0.16, 0.10 b0d3183a8dd07a222c3a2ecc2e82b05f82ed818d

---
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -0,0 +1,16 @@
+#include "cpu/exec/template-start.h"
+
+#define instr movzx
+
+static void do_execute() {
+	DATA_TYPE res = op_src->val;
+	if(op_dest->size == 4)
+		write_operand_l(op_dest, (int32_t)res);
+	if(op_dest->size == 2)
+		write_operand_w(op_dest, (int16_t)res);
+	print_asm_template2();
+}
+
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
