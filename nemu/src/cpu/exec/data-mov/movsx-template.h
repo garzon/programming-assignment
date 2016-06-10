3c6c4bfdb53c6b881243df35404ed4c1531be097
tracer-ics2015 <tracer@njuics.org>
2016-06-10 17:44:47 +0800

    >  compile NEMU 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  17:44:47 up 10 days, 12:09,  3 users,  load average: 0.26, 0.17, 0.15 fb5b02265bd17e62b46cc2a21b13e34c3d85246

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -9,7 +9,7 @@ static void do_execute() {
 	if(op_dest->size == 2)
 		write_operand_w(op_dest, (int16_t)res);
 	if(op_dest->size == 1) {
-		print_asm_template2();
+		printf("dest_size: %d", (int)op_dest->size);
 		panic("movsx_size_err");
 	}
 	print_asm_template2();
