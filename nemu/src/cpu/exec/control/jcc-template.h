f02e46cf049eb3bd89104644db923baae1f151d9
tracer-ics2015 <tracer@njuics.org>
2016-06-11 15:59:23 +0800

    >  compile NEMU 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:59:23 up 11 days, 10:24,  1 user,  load average: 0.15, 0.12, 0.13 30055921336ecbee4871bd54f31506729d16ecca

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -6,12 +6,32 @@ static void do_execute() {
 	uint8_t opcode = ops_decoded.opcode;
 	bool cond = false;
 	const char *opName;
+	bool CF = cpu.eflags_cf,
+		 ZF = cpu.eflags_zf,
+		 SF = cpu.eflags_sf,
+		 OF = cpu.eflags_of,
+		 PF = cpu.eflags_pf;
+
 	if(DATA_BYTE != 1)
 		opcode -= 0x10;
 	switch(opcode) {
 #define SUBOP(name, condition) opName = str(name); cond = (condition); break
-		case 0x74: SUBOP(JE, cpu.eflags_zf);
-		case 0x7E: SUBOP(JLE, cpu.eflags_zf || cpu.eflags_sf != cpu.eflags_of);
+		case 0x74: SUBOP(JE, ZF);
+		case 0x7E: SUBOP(JLE, ZF || SF != OF);
+		case 0x77: SUBOP(JA, !CF && !ZF);
+		case 0x73: SUBOP(JAE, !CF);
+		case 0x72: SUBOP(JB, CF);
+		case 0x76: SUBOP(JBE, CF || ZF);
+		case 0x7F: SUBOP(JG, !ZF && SF == OF);
+		case 0X7D: SUBOP(JGE, SF == OF);
+		case 0x7C: SUBOP(JL, SF != OF);
+		case 0x75: SUBOP(JNE, !ZF);
+		case 0x71: SUBOP(JNO, !OF);
+		case 0x7B: SUBOP(JNP, !PF);
+		case 0x79: SUBOP(JNS, !SF);
+		case 0x70: SUBOP(JO, OF);
+		case 0x7A: SUBOP(JP, PF);
+		case 0x78: SUBOP(JS, SF);
 		default:
 			panic("not implemented");
 #undef SUBOP
