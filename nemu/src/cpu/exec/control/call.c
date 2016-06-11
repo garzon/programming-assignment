8f5d083d07172afbb593f3f902cd48cac9940efa
tracer-ics2015 <tracer@njuics.org>
2016-06-11 16:41:59 +0800

    >  compile NEMU 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:41:59 up 11 days, 11:06,  2 users,  load average: 0.00, 0.09, 0.18 cdf33520127abc779a1b42e0c65d72bf45fd1fd

--- nemu/src/cpu/exec/control/call.c
+++ nemu/src/cpu/exec/control/call.c
@@ -3,7 +3,7 @@
 make_helper(call_rel32) {
 	swaddr_t rel32 = eip + swaddr_read(eip + 1, 4);
 	cpu.esp -= 4;
-	swaddr_write(cpu.esp, 4, eip);
+	swaddr_write(cpu.esp, 4, eip+5);
 	cpu.eip = rel32 + 4;
 	print_asm("CALL 0x%x", cpu.eip+1);
 
--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -5,12 +5,12 @@
 static void do_execute() {
 	cpu.eip = swaddr_read(cpu.esp, 4);
 	cpu.esp += 4;
-	/*if(op_src->type == OP_TYPE_IMM) {
+	if(op_src->type == OP_TYPE_IMM) {
 		cpu.esp += op_src->val;
 		print_asm_template2();
 	} else {
 		print_asm_template1();
-	}*/print_asm_template1();
+	}
 }
 
 make_instr_helper(i)
