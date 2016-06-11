136cd5724b04fbc5d93d110af3035b51f71d461b
tracer-ics2015 <tracer@njuics.org>
2016-06-11 18:02:26 +0800

    >  compile NEMU 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:02:26 up 11 days, 12:27,  2 users,  load average: 0.34, 0.30, 0.25 182e152ee4470e06e0b3ba286ded1af61acf7cfe

---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,24 @@
+#include "cpu/exec/template-start.h"
+
+#define instr adc
+
+static void do_execute() {
+	DATA_TYPE a = op_dest->val;
+	DATA_TYPE b = op_src->val;
+	DATA_TYPE res = a+b+cpu.eflags_cf;
+	cpu.eflags_cf = res < a || res < b;
+	cpu.eflags_of = (MSB(a) ^ MSB(b)) && (MSB(a) ^ MSB(res));
+	SET_PSZ_FLAGS(res);
+	OPERAND_W(op_dest, res);
+	print_asm_template2();
+}
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
