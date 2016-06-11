9dba223ee3fe8c43ab7ee5d38332481e37b6d141
tracer-ics2015 <tracer@njuics.org>
2016-06-11 20:15:26 +0800

    >  compile NEMU 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:15:26 up 11 days, 14:40,  2 users,  load average: 0.22, 0.21, 0.18 5545dba516b3fcc6913bd87a7f5e7011b1e6f5bf

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -6,7 +6,7 @@ static void do_execute() {
 	if(op_src->type == OP_TYPE_IMM) {
 		cpu.eip += (DATA_TYPE_S)op_src->val;
 		print_asm("jmp(rel) 0x%x (jumped)", cpu.eip);
-		cpu.eip -= 1 + DATA_BYTE;
+		//cpu.eip -= 1 + DATA_BYTE;
 	} else {
 		cpu.eip = op_src->val;
 		print_asm("jmp(abs) 0x%x (jumped)", cpu.eip);
