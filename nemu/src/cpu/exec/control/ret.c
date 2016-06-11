8b3abe0eccdf08398cca5edfe0f742ada633b5fc
tracer-ics2015 <tracer@njuics.org>
2016-06-11 16:24:32 +0800

    >  compile NEMU 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:24:32 up 11 days, 10:49,  1 user,  load average: 0.27, 0.28, 0.21 e463b27186dc3cd9d6d50194f7ca21882d0f2a8a

--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -14,11 +14,11 @@ make_helper_v(ret_i)
 #include "cpu/exec/template-start.h"
 #define instr ret
 make_helper(ret) {
-	cpu.eip = swaddr_read(cpu.esp, 4);
+	cpu.eip = swaddr_read(cpu.esp, 4)-1;
 	cpu.esp += 4;
 	print_asm_template1();
 
-	return 0;
+	return 1;
 }
 #include "cpu/exec/template-end.h"
 #undef DATA_BYTE
