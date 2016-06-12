6d4689e1ad5185f5d186941686cfb7ba2aa9afeb
tracer-ics2015 <tracer@njuics.org>
2016-06-12 12:12:11 +0800

    >  run 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:12:11 up 12 days,  6:36,  1 user,  load average: 0.06, 0.05, 0.09 33f4b4829644cf85d2bf8db93a109c193a2660

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -11,7 +11,7 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 	FLOAT s,h;
 	h = F_div_int((b - a), n);
 	s = F_div_int(fun(a) + fun(b), 2 );
-	for(k = 1; k < n; k ++) {
+	for(k = 1; k <= n; k ++) {
 		s += fun(a + F_mul_int(h, k));
 	}
 	s = F_mul_F(s, h);
@@ -54,7 +54,7 @@ int main() {
 	nemu_assert(Fabs(f(f2F(-1+0.4))-f2F(0.1)) < eps);
 	nemu_assert(Fabs(f(f2F(-1+1.4))-f2F(0.2)) < eps);
 */	
-	
+
 	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
 	FLOAT ans = f2F(0.551222);
 
