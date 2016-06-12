2eed822dd6aa8ea3034b6a36fb679a60119cb452
tracer-ics2015 <tracer@njuics.org>
2016-06-12 12:43:24 +0800

    >  run 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:43:24 up 12 days,  7:08,  1 user,  load average: 0.33, 0.30, 0.22 3e0af77c251d9df24887f04c3cfb64bd7b0de18

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -11,7 +11,7 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 	FLOAT s,h;
 	h = F_div_int((b - a), n);
 	s = F_div_int(fun(a) + fun(b), 2 );
-	for(k = 1; k <= n; k ++) {
+	for(k = 1; k < n; k ++) {
 		s += fun(a + F_mul_int(h, k));
 	}
 	s = F_mul_F(s, h);
