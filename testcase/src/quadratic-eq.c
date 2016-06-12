1d53702521ed669ec58e74a06a96aee40de2feab
tracer-ics2015 <tracer@njuics.org>
2016-06-12 13:32:40 +0800

    >  run 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  13:32:40 up 12 days,  7:57,  1 user,  load average: 0.27, 0.13, 0.13 246be23294da17c013a4025fbbe754ad908a4ef9

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -15,7 +15,7 @@ int main() {
 	nemu_assert(F_mul_int(a, 2) == 0x10000);
 	FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
 	FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));
-
+	set_bp();
 	FLOAT x1_ans = f2F(0.618);
 	FLOAT x2_ans = f2F(-1.618);
 
