fecc50152b4a9443c1aba649b8615fdecd3e0982
tracer-ics2015 <tracer@njuics.org>
2016-06-12 12:39:22 +0800

    >  run 13307130320 garzon Linux sixstars-XPS-8300 3.19.0-42-generic #48~14.04.1-Ubuntu SMP Fri Dec 18 10:24:49 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:39:22 up 12 days,  7:04,  1 user,  load average: 0.51, 0.25, 0.18 3cfede3c8d47e98176addbca9303faf4b6a29bad

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -21,7 +21,6 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 int main() { 
 	FLOAT eps = f2F(1e-3);
 
-/*
 	nemu_assert(f2F(12.45)-f2F(12.449999) < eps);
 
 	FLOAT aa = f2F(124.99);
@@ -39,7 +38,7 @@ int main() {
 	nemu_assert(F2int(int2F(-3263)) == -3263);
 
 	nemu_assert(Fabs(F_mul_F(bb, int2F(-1)))-bb < eps);
-*/
+
 	FLOAT x = f2F(1.0);
 	nemu_assert(x == 0x10000);
 	FLOAT A = F_mul_F(x, x);
@@ -50,9 +49,9 @@ int main() {
 	nemu_assert(Fabs(A - f2F(26.0)) < eps);
 	A = F_div_F(int2F(1), A);
 	nemu_assert(Fabs(A - f2F(1/26.0)) < eps);
-/**/	
 
-	/*nemu_assert(Fabs(f(f2F(-0.8))-f2F(0.0588235)) < eps);
+
+	nemu_assert(Fabs(f(f2F(-0.8))-f2F(0.0588235)) < eps);
 	nemu_assert(Fabs(f(f2F(-0.6))-f2F(0.1)) < eps);
 	nemu_assert(Fabs(f(f2F(-0.4))-f2F(0.2)) < eps);
 	nemu_assert(Fabs(f(f2F(-0.2))-f2F(0.5)) < eps);
@@ -61,17 +60,17 @@ int main() {
 	nemu_assert(Fabs(f(f2F(0.8))-f2F(0.0588235)) < eps);
 	nemu_assert(Fabs(f(f2F(0.6))-f2F(0.1)) < eps);
 	nemu_assert(Fabs(f(f2F(0.4))-f2F(0.2)) < eps);
-	nemu_assert(Fabs(f(f2F(0.2))-f2F(0.5)) < eps);*/
-	//nemu_assert(Fabs(f(f2F(1))-f2F(0.0384615)) < eps);
+	nemu_assert(Fabs(f(f2F(0.2))-f2F(0.5)) < eps);
+	nemu_assert(Fabs(f(f2F(1))-f2F(0.0384615)) < eps);
 	FLOAT X= f(f2F(1.0));
-	FLOAT tmp=f2F(1/26.0);
-	nemu_assert(Fabs(X-tmp) < eps);
+	FLOAT ttmp=f2F(1/26.0);
+	nemu_assert(Fabs(X-ttmp) < eps);
 
-	/*FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
+	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
 	FLOAT ans = f2F(0.551222);
 
 	FLOAT diff = a - ans;
-	nemu_assert(Fabs(diff) < eps);*/
+	nemu_assert(Fabs(diff) < eps);
 	HIT_GOOD_TRAP;
 	return 0;
 }
